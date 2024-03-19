a9011e4e6d5ec2de3da22a3224c5d7b47d787641
AlnaSher <shershnevahelen@gmail.com>
2024-03-19 03:11:23 +0300

    remove unnecessary

--- src/main/java/com/example/courses/model/Lesson.java
+++ src/main/java/com/example/courses/model/Lesson.java
@@ -1,7 +1,5 @@
 package com.example.courses.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/example/courses/model/Student.java
+++ src/main/java/com/example/courses/model/Student.java
@@ -1,6 +1,5 @@
 package com.example.courses.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
